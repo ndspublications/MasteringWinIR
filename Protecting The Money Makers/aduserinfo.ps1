f39cb41cf366b42a48a2e24d77a0291141a84e2b
ndspublications <publications@networkdefensesolutions.com>
2025-12-20 19:30:30 -0500

    Update aduserinfo.ps1
    Export AD user accounts to CSV excluding disabled accounts.

--- Protecting The Money Makers/aduserinfo.ps1
+++ Protecting The Money Makers/aduserinfo.ps1
@@ -1,3 +1,3 @@
-
 Get-ADUser -Filter * -Properties DisplayName, Title, EmailAddress |
-Select-Object DisplayName, Title, EmailAddress
+Select-Object DisplayName, Title, EmailAddress |
+Export-Csv "C:\AD_Users_Name_Title_Email.csv" -NoTypeInformation -Encoding UTF8
